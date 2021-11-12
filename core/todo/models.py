c6b6c7bba98fd77222814c8c1a443703a2a228d0
DenisDanchyk <danchyk602@gmail.com>
2021-11-12 10:44:04 +0200

    Feat: Created TodoTask model

--- core/todo/models.py
+++ core/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TodoTask(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Автор")
+    task_is_set_to = models.ManyToManyField(
+        User,
+        related_name='user_tasks',
+        verbose_name="Кому поставлена задача"
+    )
+    created = models.DateField(auto_now_add=True, verbose_name="Створено")
+    updated = models.DateField(auto_now=True, verbose_name="Оновлено")
+    image = models.ImageField(upload_to='task_images/',
+                              verbose_name="Фотографія")
